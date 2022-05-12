ce6b84942986570a826a4a8dbf81cc0694976892
dev-geov <geapsi@gmail.com>
2022-05-12 18:50:25 -0300

    UPDATE: adjusts name for s3 bucket

--- first_code.tf
+++ first_code.tf
@@ -3,7 +3,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
-    bucket = "tf_course_dev-geov"
-    acl = "private"
+resource "aws_s3_bucket" "dev_geov_tf_course" {
+    bucket = "tf-course-dev-geov"
+    aws_s3_bucket_acl = "private"
 }
