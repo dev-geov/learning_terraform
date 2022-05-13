114c7fdf249f3fcd396608426ae4c03a7ae6c685
dev-geov <geapsi@gmail.com>
2022-05-12 22:00:41 -0300

    ADD: set resource for ip address

--- prod.tf
+++ prod.tf
@@ -1,13 +1,62 @@
 provider "aws" {
     profile = "default"
-    region = "us-east-1"
+    region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
     bucket = "prod-tf-course-20220512"
-    acl = "private"
+    acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "prod_web" {
+    name        = "prod_web"
+    description = "Allow standard http and https ports inbound and everything outbound"
+
+    ingress {
+        cidr_blocks      = [ "0.0.0.0/0" ]
+        from_port        = 80
+        protocol         = "tcp"
+        to_port          = 80
+    }
+    ingress {
+        cidr_blocks      = [ "0.0.0.0/0" ]
+        from_port        = 443
+        protocol         = "tcp"
+        to_port          = 443
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    tags = {
+      "Terraform": "true"
+    }
+}
+
+resource "aws_instance" "prod_web" {
+    ami = "ami-065fb54436c0e2d57"
+    instance_type = "t3.nano"
     
+    vpc_security_group_ids = [
+        aws_security_group.prod_web.id
+    ]
+
+    tags = {
+      "Terraform": "true"
+    }
+}
+
+resource "aws_eip" "prod_web" {
+    instance = aws_instance.prod_web.id
+    
+    tags = {
+      "Terraform": "true"
+    }
 }
