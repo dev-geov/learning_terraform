b6d74a39a2cb19a7692060df6a814a36287d0644
dev-geov <geapsi@gmail.com>
2022-05-12 21:03:51 -0300

    REMOVE: renamed file

--- first_code.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "dev_geov_tf_course" {
-    bucket = "tf-course-dev-geov"
-    aws_s3_bucket_acl = "private"
-}
