5377d3f72a2b498407709e5322dc39444f46591e
dev-geov <geapsi@gmail.com>
2022-05-13 23:02:30 -0300

    ADD: variables

--- prod.tf
+++ prod.tf
@@ -1,3 +1,22 @@
+variable "whitelist" {
+    type = list(string)
+}               
+variable "web_image_id" {
+    type = string
+}            
+variable "web_instance_type" {
+    type = string
+}       
+variable "web_desired_capacity" {
+    type = number
+}    
+variable "web_max_size" {
+    type = number
+}            
+variable "web_min_size" {
+    type = number
+}   
+
 provider "aws" {
     profile = "default"
     region  = "us-east-1"
@@ -12,18 +31,34 @@ resource "aws_default_vpc" "default" {
 
 }
 
+resource "aws_default_subnet" "default_az1" {
+    availability_zone = "us-east-1"
+    tags = {
+        "Terraform": "true"
+    }
+  
+}
+
+resource "aws_default_subnet" "default_az2" {
+    availability_zone = "us-east-2"
+    tags = {
+        "Terraform": "true"
+    }
+  
+}
+
 resource "aws_security_group" "prod_web" {
     name        = "prod_web"
     description = "Allow standard http and https ports inbound and everything outbound"
 
     ingress {
-        cidr_blocks      = [ "0.0.0.0/0" ]
+        cidr_blocks      = var.whitelist
         from_port        = 80
         protocol         = "tcp"
         to_port          = 80
     }
     ingress {
-        cidr_blocks      = [ "0.0.0.0/0" ]
+        cidr_blocks      = var.whitelist
         from_port        = 443
         protocol         = "tcp"
         to_port          = 443
@@ -32,7 +67,7 @@ resource "aws_security_group" "prod_web" {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = [ "0.0.0.0/0" ]
+        cidr_blocks = var.whitelist
     }
 
     tags = {
@@ -40,30 +75,48 @@ resource "aws_security_group" "prod_web" {
     }
 }
 
-resource "aws_instance" "prod_web" {
-    count         = 2
+resource "aws_elb" "prod_web" {
+    name            = "prod-web"
+    subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+    security_groups = [aws_security_group.prod_web.id]
 
-    ami           = "ami-065fb54436c0e2d57"
-    instance_type = "t3.nano"
-    
-    vpc_security_group_ids = [
-        aws_security_group.prod_web.id
-    ]
+    listener {
+        instance_port     = 80
+        instance_protocol = "http"
+        lb_port           = 80
+        lb_protocol       = "http"
+    }
 
     tags = {
         "Terraform": "true"
     }
 }
 
-resource "aws_eip_association" "prod_web" {
-    instance_id   = aws_instance.prod_web.0.id
-    allocation_id = aws_eip.prod_web.id
+resource "aws_launch_template" "prod_web" {
+    name_prefix   = "prod-web"
+    image_id      = var.web_image_id
+    instance_type = var.web_instance_type
 }
 
-resource "aws_eip" "prod_web" {
-    instance = aws_instance.prod_web.id
+resource "aws_autoscaling_group" "prod_web" {
+    vpc_zone_identifier = [ aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
+    desired_capacity    = var.web_desired_capacity
+    max_size            = var.web_max_size
+    min_size            = var.web_min_size
 
-    tags = {
-        "Terraform": "true"
+    launch_template {
+        id      = aws_launch_template.prod_web.id
+        version = "$Latest"
     }
+
+    tag {
+        key                 = "Terraform"
+        value               = "true"
+        propagate_at_launch = "true"
+    }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+    autoscaling_group_name = aws_autoscaling_group.prod_web.id
+    elb                    = aws_elb.prod_web.id
 }
